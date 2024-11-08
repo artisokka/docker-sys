933c6fba82c25aad61aa5dba9fd91911d26a8d0a
Janika <janika.l.ranta@tuni.fi>
2024-11-08 22:30:49 +0200

    Submission for exercise4

--- Service1/app.py
+++ Service1/app.py
@@ -1,39 +1,45 @@
-import os
 import socket
 import subprocess
-import time
-import requests
-from flask import Flask, jsonify
+import docker
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-def get_system_info():
-    # Get specs
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/request-data')
+def request_data():
     ip_address = socket.gethostbyname(socket.gethostname())
     processes = subprocess.getoutput("ps -ax")
     disk_space = subprocess.getoutput("df -h /")
     uptime = subprocess.getoutput("uptime -p")
-
-    return {
+    
+    data = {
         "IP Address": ip_address,
         "Processes": processes,
         "Disk Space": disk_space,
         "Uptime": uptime
     }
+    return jsonify(data)
 
-@app.route('/')
-def get_info():
-    service1_info = get_system_info()
-    
-    # Request to Service2
-    service2_response = requests.get("http://service2:5001/system-info")
-    service2_info = service2_response.json() if service2_response.ok else {"error": "Could not get data from Service2"}
-    
-    combined_info = {
-        "Service1": service1_info,
-        "Service2": service2_info
-    }
-    return jsonify(combined_info)
+@app.route('/stop', methods=['POST'])
+def stop_service():
+    try:
+        client = docker.from_env()
+        containers = client.containers.list()
+        
+        for container in containers:
+            if "service1-" in container.name:
+                container.stop()
+            elif "service2-" in container.name:
+                container.stop()
+            elif "nginx-" in container.name:
+                container.stop()
 
+    except subprocess.CalledProcessError as e:
+        return f"Error stopping containers: {e}", 500
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8199)
